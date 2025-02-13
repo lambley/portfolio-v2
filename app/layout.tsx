80e50f6729891df30769e194c40afecfc029c945
Aaron Lambley <aaron.lambley@gmail.com>
2025-02-13 11:46:44 +0000

    Add navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navigation from "@/components/navigation";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body
         className="w-3/4 m-auto bg-main text-text"
       >
+        <Navigation />
         {children}
       </body>
     </html>
---
+++ components/navigation.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+import { cn } from "@/lib/utils";
+
+const blogPosts = [
+  { title: "Post 1", href: "/blog/post-1" },
+  { title: "Post 2", href: "/blog/post-2" },
+  { title: "Post 3", href: "/blog/post-3" },
+];
+
+const portfolioItems = [
+  { title: "Project 1", href: "/portfolio/project-1" },
+  { title: "Project 2", href: "/portfolio/project-2" },
+  { title: "Project 3", href: "/portfolio/project-3" },
+];
+
+export default function NavigationMenuDemo() {
+  return (
+    <div className="w-fit flex justify-center mx-auto lg:mt-4 lg:mb-0 lg:sticky lg:top-4 mb-4 bottom-0 absolute left-0 right-0">
+      <NavigationMenu className="z-[5] max-w-[1200px] w-full px-4">
+        <NavigationMenuList className="flex justify-between w-full">
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                <FontAwesomeIcon icon={faHouse} />
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="m750:max-w-[80px] m750:text-xs">
+              Blog
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[200px] gap-3 p-4">
+                {blogPosts.map((post) => (
+                  <ListItem
+                    key={post.title}
+                    title={post.title}
+                    href={post.href}
+                  >
+                    {post.title}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="m750:max-w-[80px] m750:text-xs">
+              Portfolio
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[200px] gap-3 p-4">
+                {portfolioItems.map((item) => (
+                  <ListItem
+                    key={item.title}
+                    title={item.title}
+                    href={item.href}
+                  >
+                    {item.title}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "hover:bg-accent block text-mtext select-none space-y-1 rounded-base border-2 border-transparent p-3 leading-none no-underline outline-none transition-colors hover:border-border dark:hover:border-darkBorder",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-base font-heading leading-none">{title}</div>
+          <p className="text-muted-foreground font-base line-clamp-2 text-sm leading-snug">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
