d60b2f5d9dafdf26377a00b1bc4ef2224ecb972d
Aaron Lambley <aaron.lambley@gmail.com>
2025-02-14 15:54:41 +0000

    Remove prose class from projects layout for simplified styling

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,3 +1,3 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
-  return <div className="prose max-w-none my-16">{children}</div>;
+  return <div className="my-16">{children}</div>;
 }
