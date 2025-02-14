cf965693ffcffab8d1106b9656a1f25b4a772f81
Aaron Lambley <aaron.lambley@gmail.com>
2025-02-14 14:16:27 +0000

    Move blog fetching to lib

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,53 +1,8 @@
 import Link from "next/link";
-
-async function fetchPosts() {
-  const posts = [
-    {
-      id: 1,
-      title: "Part 1: How to Build a Ruby on Rails and ReactTS App",
-      slug: "post-1",
-    },
-    {
-      id: 2,
-      title: "Part 2: How to Build a Ruby on Rails and ReactTS App",
-      slug: "post-2",
-    },
-    {
-      id: 3,
-      title: "Part 3: How to Build a Ruby on Rails and ReactTS App",
-      slug: "post-3",
-    },
-    {
-      id: 4,
-      title: "Part 4: How to Build a Ruby on Rails and ReactTS App",
-      slug: "post-4",
-    },
-    {
-      id: 5,
-      title: "How to setup reCAPTCHA v2 in a Rails and React app",
-      slug: "post-5",
-    },
-    {
-      id: 6,
-      title: "How to get started with GitHub Actions",
-      slug: "post-6",
-    },
-    {
-      id: 7,
-      title: "Design Patterns - a few I have used recently",
-      slug: "post-7",
-    },
-    {
-      id: 8,
-      title: "Getting started with testing in Ruby on Rails",
-      slug: "post-8",
-    },
-  ];
-  return posts;
-}
+import { fetchBlogs } from "@/lib/blogs";
 
 export default async function Blog() {
-  const posts = await fetchPosts();
+  const posts = await fetchBlogs();
 
   return (
     <main className="w-full p-4 flex flex-col items-center justify-center bg-main text-text">
---
+++ lib/blogs.ts
@@ -0,0 +1,45 @@
+export async function fetchBlogs() {
+  const posts = [
+    {
+      id: 1,
+      title: "Part 1: How to Build a Ruby on Rails and ReactTS App",
+      slug: "post-1",
+    },
+    {
+      id: 2,
+      title: "Part 2: How to Build a Ruby on Rails and ReactTS App",
+      slug: "post-2",
+    },
+    {
+      id: 3,
+      title: "Part 3: How to Build a Ruby on Rails and ReactTS App",
+      slug: "post-3",
+    },
+    {
+      id: 4,
+      title: "Part 4: How to Build a Ruby on Rails and ReactTS App",
+      slug: "post-4",
+    },
+    {
+      id: 5,
+      title: "How to setup reCAPTCHA v2 in a Rails and React app",
+      slug: "post-5",
+    },
+    {
+      id: 6,
+      title: "How to get started with GitHub Actions",
+      slug: "post-6",
+    },
+    {
+      id: 7,
+      title: "Design Patterns - a few I have used recently",
+      slug: "post-7",
+    },
+    {
+      id: 8,
+      title: "Getting started with testing in Ruby on Rails",
+      slug: "post-8",
+    },
+  ];
+  return posts;
+}
