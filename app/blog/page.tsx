3342f3b3b7cd412cefb70f6e3a4072fa4d6da11f
Aaron Lambley <aaron.lambley@gmail.com>
2025-02-17 11:24:27 +0000

    Implement sorting toggle for blog posts and refactor data fetching

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import type { Blog } from "@/types/shared";
 import { fetchBlogs } from "@/lib/blogs";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 
-export default async function Blog() {
-  const posts: Blog[] = await fetchBlogs();
+export default function Blog() {
+  const [posts, setPosts] = useState<Blog[]>([]);
+  const [isSortedByNewest, setIsSortedByNewest] = useState(true);
+
+  useEffect(() => {
+    async function getBlogs() {
+      const fetchedBlogs = await fetchBlogs();
+      setPosts(fetchedBlogs);
+    }
+    getBlogs();
+  }, []);
+
+  const handleToggle = (checked: boolean) => {
+    setIsSortedByNewest(checked);
+  };
+
+  const sortedPosts = isSortedByNewest
+    ? [...posts].sort(
+        (a, b) =>
+          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      )
+    : posts;
 
   return (
     <main className="w-full p-4 flex flex-col items-center justify-center bg-main text-text">
@@ -12,8 +36,18 @@ export default async function Blog() {
         A few articles and guides I have put together in my Web Development
         journey.
       </p>
+      <div className="flex items-center space-x-2">
+        <Switch
+          id="sort-by-newest"
+          checked={isSortedByNewest}
+          onChange={handleToggle}
+        />
+        <Label htmlFor="sort-by-newest">
+          {isSortedByNewest ? "Latest" : "Oldest"}
+        </Label>
+      </div>
       <ul className="mt-4 list-disc">
-        {posts.map((post) => (
+        {sortedPosts.map((post) => (
           <li key={post.id}>
             <Link href={`/blog/${post.slug}`}>{post.title}</Link>
           </li>
